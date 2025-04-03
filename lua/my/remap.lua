f406975b48725d5cef9ab2d47d3a032a58959521
ErPollo07 <nicola.tosatti@iisviolamarchesini.edu.it>
2025-04-03 15:38:29 +0200

    ADD keymap to navigate throught nvim panes

--- lua/my/remap.lua
+++ lua/my/remap.lua
@@ -1,18 +1,22 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<leader>E", vim.cmd.Ex) 
+vim.keymap.set("n", "<leader>E", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>L", vim.cmd.Lazy)
 vim.keymap.set("n", "<leader>Q", vim.cmd.q)
 
 -- Remap movment keys
 vim.keymap.set("n", "j", "h")
 vim.keymap.set("n", "k", "j")
-vim.keymap.set("n", "l", "k") 
+vim.keymap.set("n", "l", "k")
 vim.keymap.set("n", ";", "l")
 
 vim.keymap.set("v", "j", "h")
 vim.keymap.set("v", "k", "j")
-vim.keymap.set("v", "l", "k") 
+vim.keymap.set("v", "l", "k")
 vim.keymap.set("v", ";", "l")
 
-
+-- Navigation throught nvim panes
+vim.keymap.set('n', '<c-k>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-;>', ':wincmd l<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd k<CR>')
