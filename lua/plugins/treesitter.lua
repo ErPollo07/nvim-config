ea15bd24f9c238caa4519349085cf8744541d323
ErPollo07 <nicola.tosatti@iisviolamarchesini.edu.it>
2025-04-06 15:05:54 +0200

    DELETE ensure_install = { 'javascript' } line in treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,6 @@ return {
 
     configs.setup({
       auto_install = true,
-      ensure_install = { "javascript" },
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false, -- For Catppuccin
