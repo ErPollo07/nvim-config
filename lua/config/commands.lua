f262ddff284b04943c916feb6fc15f09a02ee34b
ErPollo07 <nicola.tosatti@iisviolamarchesini.edu.it>
2025-04-08 15:58:45 +0200

    FIX OpenConfig command for WINDOWS

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -11,12 +11,19 @@ vim.api.nvim_create_user_command(
   "OpenConfig",
   function()
     -- 1. open a terminal instance
-    local config_folder = vim.fn.has("win32") == 1 and "C:/appdata/local/nvim" or "~/.config/nvim"
+    local terminal_cmd = "start cmd /k nvim C:/users/utente/appdata/local/nvim/"
 
-    -- 2. navigate to the configuration folder open it in nvim
-    local terminal_cmd = vim.fn.has("win32") == 1 and "start cmd.exe /K cd " .. config_folder .. " && nvim ." or "cd " .. config_folder .. " && exec $SHELL -i && nvim ."
-
-    vim.cmd("term " .. terminal_cmd)
+    os.execute(terminal_cmd)
   end,
   { desc = "Open the configuration folder" }
+)
+
+vim.api.nvim_create_user_command(
+  "NpmRunStart",
+  function()
+    local term_cmd = "start cmd /k npm run start"
+
+    os.execute(term_cmd)
+  end,
+  { desc = "Run the npm start script" }
 )
