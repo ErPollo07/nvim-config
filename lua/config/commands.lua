eb7eb9b88fcc647a8cc1e64409d45e63f2d3ff30
ErPollo07 <nicola.tosatti@iisviolamarchesini.edu.it>
2025-04-09 14:33:10 +0200

    ADD OpenConfig support for Linux and MacOS

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -1,23 +1,26 @@
 vim.api.nvim_create_user_command(
   "Ping",
   function()
-    print("pong")
+    print("Pong")
   end,
   { desc = "Test command" }
 )
 
--- Open the configuration folder '~/.config/nvim' or 'C:/appdata/local/nvim'
+-- Open the configuration folder
 vim.api.nvim_create_user_command(
   "OpenConfig",
   function()
-    -- 1. open a terminal instance
-    local terminal_cmd = "start cmd /k nvim C:/users/utente/appdata/local/nvim/"
+    local terminal_cmd = "start cmd /k nvim ~/appdata/local/nvim/" -- for Windows
+    -- local terminal_cmd = "gnome-terminal -- nvim ~/.config/nvim/" -- for Linux
+    -- local terminal_cmd = "open -a Terminal "`pwd`" && nvim ~/.config/nvim/" -- for MacOS NOT TESTED
 
     os.execute(terminal_cmd)
   end,
   { desc = "Open the configuration folder" }
 )
 
+-- Start the strart script of the project
+-- For electron projects
 vim.api.nvim_create_user_command(
   "NpmRunStart",
   function()
